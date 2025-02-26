49be9c9bd7a65e68ab3f2ff064945e059473f869
paapa-kusi <166747979+paapa-kusi@users.noreply.github.com>
2025-02-26 15:26:23 -0500

    created login route for auth, initialized overall backend

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,52 @@
-// backend
-console.log("Nodemon Test");
+// imports
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import cors from 'cors';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import bcrypt from 'bcryptjs';
+
+dotenv.config();
+
+// express app
+const app = express();
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use(express.json());
+
+// mongo connection
+// const uri = process.env.MONGO_URI;
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
+//     .then(() => console.log('Connected to MongoDB'))
+//     .catch((err) => {
+//         console.log('Failed to connect to MongoDB', err);
+//         process.exit(1);
+//     })
+
+
+// schema (rami)
+
+
+// login route
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        // check if user exists
+        const user = await User.findOne({ username });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+        const token = jwt.sign({username}, process.env.JWT_SECRET, {expiresIn: '1h'});
+        res.json({username: user.username, token});
+    }
+    catch (err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+// server
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
