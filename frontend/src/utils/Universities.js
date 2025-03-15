7b82aa48fd2892acb700991501383ba7d1ca66d1
Rami Darwiche <ramiadarwiche@gmail.com>
2025-03-15 00:36:39 -0400

    moved universities to utils

---
+++ frontend/src/utils/Universities.js
@@ -0,0 +1,68 @@
+const universities = [
+  {
+    name: "University of Florida",
+    img: "https://1000logos.net/wp-content/uploads/2017/02/florida-gators-logo-500x346.jpg",
+  },
+  {
+    name: "Alabama State University",
+    img: "https://1000logos.net/wp-content/uploads/2019/09/Alabama-State-Hornets-Logo-tumb.png",
+  },
+  {
+    name: "Florida Atlantic University",
+    img: "https://1000logos.net/wp-content/uploads/2022/02/Florida-Atlantic-Owls-logo-tumb.jpg",
+  },
+  {
+    name: "Georgia State University",
+    img: "https://1000logos.net/wp-content/uploads/2019/08/Georgia-State-Panthers.jpg",
+  },
+  {
+    name: "University of Houston",
+    img: "https://1000logos.net/wp-content/uploads/2022/02/Houston-Cougars-logo-tumb.jpg",
+  },
+  {
+    name: "Louisiana State University",
+    img: "https://1000logos.net/wp-content/uploads/2019/06/LSU-Logo.jpg",
+  },
+  {
+    name: "University of Miami",
+    img: "https://1000logos.net/wp-content/uploads/2018/01/Miami-Hurricanes-Logo-500x281.jpg",
+  },
+  {
+    name: "Michigan State University",
+    img: "https://1000logos.net/wp-content/uploads/2018/01/michigan-state-logo-500x400.jpg",
+  },
+  {
+    name: "Ohio University",
+    img: "https://1000logos.net/wp-content/uploads/2019/12/Ohio-Bobcats-Logo-tumb.png",
+  },
+  {
+    name: "Oklahoma University",
+    img: "https://1000logos.net/wp-content/uploads/2019/09/0026_Oklahoma-Sooners.jpg",
+  },
+  {
+    name: "University of Kentucky",
+    img: "https://1000logos.net/wp-content/uploads/2018/01/Kentucky-Logo-500x409.jpg",
+  },
+  {
+    name: "University of South Florida",
+    img: "https://1000logos.net/wp-content/uploads/2020/02/South-Florida-Bulls-Logo-tumb.png",
+  },
+  {
+    name: "Stanford University",
+    img: "https://1000logos.net/wp-content/uploads/2019/10/0002_Stanford-Cardinal.jpg",
+  },
+  {
+    name: "Texas A&M University",
+    img: "https://1000logos.net/wp-content/uploads/2022/02/Texas-AM-Aggies-logo-tumb.jpg",
+  },
+  {
+    name: "Tulane University",
+    img: "https://1000logos.net/wp-content/uploads/2021/07/Tulane-Green-Wave-logo-tumb.jpg",
+  },
+  {
+    name: "University of Massachusetts",
+    img: "https://1000logos.net/wp-content/uploads/2022/02/Massachusetts-Minutemen-logo-tumb.jpg",
+  },
+];
+
+export { universities };
