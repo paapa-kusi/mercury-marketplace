cd8bae0c743157741618492d5e476d4ab4bfda3e
mfahd308 <mfahd@ufl.edu>
2025-03-21 22:56:54 -0400

    Added information on home page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,24 +1,21 @@
 import Header from "@/components/layouts/Header";
 import MarqueeDivider from "../components/MarqueeDivider";
 import Hero from "@/components/Hero";
+import HomeInfo from "@/components/HomeInfo";
 import Footer from "@/components/layouts/Footer";
 
-export default function HomePage() {
+export default function HomePage() { 
   return (
     <>
       <Header />
       <div className="min-h-screen flex flex-col">
         <div className="flex-1">
           <Hero />
-          <div className="max-w-[1360px] mx-auto">
+          <div className="max-w-[1360px] mx-auto px-4">
             <MarqueeDivider />
+          <HomeInfo />
+            
 
-            <section id="about1" className="h-[60vh]">
-              <h1>About section 1</h1>
-            </section>
-            <section className="h-[60vh]">
-              <h1>About section 2</h1>
-            </section>
             <Footer />
           </div>
         </div>
