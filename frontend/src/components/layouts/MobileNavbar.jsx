f08e4c10866d425d3a3e60c35df63d13520c321b
mfahd308 <mfahd@ufl.edu>
2025-04-20 17:27:47 -0400

    Update MobileNavbar

--- frontend/src/components/layouts/MobileNavbar.jsx
+++ frontend/src/components/layouts/MobileNavbar.jsx
@@ -9,6 +9,7 @@ import {
 import { Separator } from "../ui/separator";
 import { Menu } from "lucide-react";
 import Link from "next/link";
+import { SignedOut, SignedIn, UserButton } from "@clerk/nextjs";
 
 // TODO: update nav content, use dropdowns
 const MobileNavbar = () => {
@@ -23,7 +24,7 @@ const MobileNavbar = () => {
       >
         <SheetHeader>
           <SheetTitle className="text-5xl text-[#e0e1dd]">
-            Mecury Marketplace
+            <Link href="/">Mecury Marketplace</Link>
           </SheetTitle>
           <SheetDescription className="text-lg text-gray-400">
             Browse Linklet
@@ -32,27 +33,49 @@ const MobileNavbar = () => {
         <Separator />
         <nav className="flex flex-col flex-1 gap-3 py-6 px-3 text-4xl text-[#e0e1dd]">
           <Link
-            href="/"
+            href="/store"
             className="transition-colors hover:bg-[#e0e1dd] hover:text-[#1B263B] rounded-md px-2 py-3"
           >
-            Home
+            Store
           </Link>
           <Link
-            href="/about"
+            href="/list/manage"
             className="transition-colors hover:bg-[#e0e1dd] hover:text-[#1B263B] rounded-md px-2 py-3"
           >
-            About
+            Listings
           </Link>
           <Link
-            href="/contact"
+            href="/university"
             className="transition-colors hover:bg-[#e0e1dd] hover:text-[#1B263B] rounded-md px-2 py-3"
           >
-            Contact
+            Universities
           </Link>
         </nav>
         <Separator />
         <div className="flex py-6 gap-4 px-5 text-4xl items-center">
-          sign up stuff
+          <SignedOut>
+            <div className="flex gap-x-3">
+              <Link href="/sign-in">
+                <button className="iconButton">
+                  <span className="tooltip text-md hover:text-white transition-colors">
+                    Sign In
+                  </span>
+                </button>
+              </Link>
+
+              <Link href="/sign-up">
+                <button className="iconButton">
+                  <span className="tooltip text-md hover:text-white transition-colors">
+                    Sign Up
+                  </span>
+                </button>
+              </Link>
+            </div>
+          </SignedOut>
+          {/*  TODO: allow for interactivity in mobile */}
+          <SignedIn>
+            <UserButton showName />
+          </SignedIn>
         </div>
       </SheetContent>
     </Sheet>
